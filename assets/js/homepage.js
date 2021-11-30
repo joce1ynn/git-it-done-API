c569739242b7e0819a59dc63f284aca36dfc279b
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-30 09:39:56 -0700

    add displayed user data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+// 6.1 get git api
 var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,9 +6,77 @@ var getUserRepos = function (user) {
   // make a request to the url
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      displayRepos(data, user);
     });
   });
 };
 
-getUserRepos("joce1ynn");
+// 6.2 Handle Form Submission
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+// 6.2.5 display repos
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var displayRepos = function (repos, searchTerm) {
+  // clear old content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/ " + repos[i].name;
+
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    // 6.2.5 Display Repo Issues
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" +
+        repos[i].open_issues_count +
+        " issue(s)";
+    } else {
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
